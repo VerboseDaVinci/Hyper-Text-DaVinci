dfb1ba437564ef966d3236aae6c09d91e59b06f1
gabrieldeml <gabrielddeml@gmail.com>
2021-01-23 14:25:07 -0500

    👾 WE have emojis ✌

--- convert_image.js
+++ convert_image.js
@@ -1,1 +1,14 @@
 // Make image simple
+
+function convertImage(img) {
+    let dst = new cv.Mat();
+    cv.cvtColor(img, dst, cv.COLOR_RGBA2GRAY, 0);
+    cv.threshold(255, dis)
+    //cv2.threshold(img, thresh, 255, cv2.THRESH_BINARY)[1]
+    // 👾
+    return dst
+}
+
+function helloworld() {
+    hello()
+}
