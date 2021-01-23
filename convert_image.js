b938bb380434dfa1503144c604e4121b4cd1d93b
gabrieldeml <gabrielddeml@gmail.com>
2021-01-23 14:59:58 -0500

    Can break apart the image into chanels

--- convert_image.js
+++ convert_image.js
@@ -1,13 +1,20 @@
 // Make image simple
 
 function convertImage(img) {
-    let dst = new cv.Mat();
-    let dst2 = new cv.Mat();
-    cv.cvtColor(img, dst, cv.COLOR_RGBA2GRAY, 0);
-    cv.threshold(dst, dst2, 128, 255, cv.THRESH_BINARY);
-    return dst2
-}
+    let dstr = new cv.Mat();
+    let dstg = new cv.Mat();
+    let dstb = new cv.Mat();
+    let rgbaPlanes = new cv.MatVector();
+    cv.split(img, rgbaPlanes);
 
-function helloworld() {
-    hello()
+
+    // cv.cvtColor(img, dst, cv.COLOR_RGBA2GRAY, 0);
+    // cv.inRange(img, low, high, dst);
+    cv.threshold(rgbaPlanes.get(0), dstr, 128, 255, cv.THRESH_BINARY);
+    cv.threshold(rgbaPlanes.get(1), dstg, 128, 255, cv.THRESH_BINARY);
+    cv.threshold(rgbaPlanes.get(2), dstb, 128, 255, cv.THRESH_BINARY);
+    cv.imshow('canvasR', dstr);
+    cv.imshow('canvasG', dstg);
+    cv.imshow('canvasB', dstb);
+    return dstr
 }
